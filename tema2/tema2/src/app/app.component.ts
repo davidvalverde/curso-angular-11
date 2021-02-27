ad31b345d24cb60c817b44add0e14b9cafa8be13
dmvalverde <dmvalverde@profile.es>
2021-02-27 18:52:47 +0100

    Tema 2 - Introducción a Angular

---
+++ tema2/tema2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tema 2: Introducción a Angular';
+  nombre = 'David';
+}
